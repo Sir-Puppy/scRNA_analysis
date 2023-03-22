3166384701f049d4ea21c3907b02e702ea146d14
Ian Costa Paixao <84053730+Sir-Puppy@users.noreply.github.com>
2023-03-22 15:57:10 +0900

    Update QC_Analysis.R

--- QC_Analysis.R
+++ QC_Analysis.R
@@ -131,8 +131,37 @@ merged_seurat@meta.data <- metadata
 # Create .RData object to load at any time
 save(merged_seurat, file="data/merged_filtered_seurat.RData")
 
+#Filtering##
+
+# Filter out low quality cells using selected thresholds - these will change with experiment
+filtered_seurat <- subset(x = merged_seurat, 
+                          subset= (nUMI >= 500) & 
+                            (nGene >= 250) & 
+                            (log10GenesPerUMI > 0.80) & 
+                            (mitoRatio < 0.20))
+
+# Extract counts
+counts <- GetAssayData(object = filtered_seurat, slot = "counts")
+
+# Output a logical matrix specifying for each gene on whether or not there are more than zero counts per cell
+nonzero <- counts > 0
+
+# Sums all TRUE values and returns TRUE if more than 10 TRUE values per gene
+keep_genes <- Matrix::rowSums(nonzero) >= 10
+
+# Only keeping those genes expressed in more than 10 cells
+filtered_counts <- counts[keep_genes, ]
+
+# Reassign to filtered Seurat object
+filtered_seurat <- CreateSeuratObject(filtered_counts, meta.data = filtered_seurat@meta.data)
+
+# Save filtered subset to new metadata
+metadata_clean <- filtered_seurat@meta.data
+
+save(filtered_seurat, file="data/seurat_filtered.RData")
+
 # Visualize the number of cell counts per sample
-metadata %>% 
+metadata_clean %>% 
   ggplot(aes(x=sample, fill=sample)) + 
   geom_bar() +
   theme_classic() +
@@ -141,7 +170,7 @@ metadata %>%
   ggtitle("NCells")
 
 # Visualize the number UMIs/transcripts per cell
-metadata %>% 
+metadata_clean %>% 
   ggplot(aes(color=sample, x=nUMI, fill= sample)) + 
   geom_density(alpha = 0.2) + 
   scale_x_log10() + 
@@ -151,7 +180,7 @@ metadata %>%
 
 
 # Visualize the distribution of genes detected per cell via histogram
-metadata %>% 
+metadata_clean %>% 
   ggplot(aes(color=sample, x=nGene, fill= sample)) + 
   geom_density(alpha = 0.2) + 
   theme_classic() +
@@ -159,14 +188,14 @@ metadata %>%
   geom_vline(xintercept = 300)
 
 # Visualize the overall complexity of the gene expression by visualizing the genes detected per UMI (novelty score)
-metadata %>%
+metadata_clean %>%
   ggplot(aes(x=log10GenesPerUMI, color = sample, fill=sample)) +
   geom_density(alpha = 0.2) +
   theme_classic() +
   geom_vline(xintercept = 0.8)
 
 # Visualize the distribution of mitochondrial gene expression detected per cell
-metadata %>% 
+metadata_clean %>% 
   ggplot(aes(color=sample, x=mitoRatio, fill=sample)) + 
   geom_density(alpha = 0.2) + 
   scale_x_log10() + 
@@ -174,7 +203,7 @@ metadata %>%
   geom_vline(xintercept = 0.2)
 
 # Visualize the correlation between genes detected and number of UMIs and determine whether strong presence of cells with low numbers of genes/UMIs
-metadata %>% 
+metadata_clean %>% 
   ggplot(aes(x=nUMI, y=nGene, color=mitoRatio)) + 
   geom_point() + 
   scale_colour_gradient(low = "gray90", high = "black") +
